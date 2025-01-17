d8218a11c792e7ea963d1361d3f6a48e2566dfd9
gilangmgm98 <gilangmgm98@gmail.com>
2025-01-17 20:50:06 +0700

    baru sampai custom provider

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,99 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Header, HostParam, HttpCode, HttpRedirectResponse, Ip, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UserService } from './user.service';
 
 @Controller('/api/users')
 export class UserController {
+    constructor(private service: UserService) { }
+    
+    @Get('/helloService')
+    sayHelloService(
+        @Query('first_name') first_name: string,
+        @Query('last_name') last_name: string
+    ): string {
+        return this.service.sayHello(first_name || last_name)
+    }
+
+    @Get('/view/hello')
+    viewHello(
+        @Query('name') name: string,
+        @Res() response: Response,
+    ) {
+        response.render('index.html', {
+            title: 'Template Engine Mustache',
+            name: name
+        })
+        // response.status(200).json({
+        //     success: true,
+        //     message: `Hello, ${name}!`
+        // });
+    }
+
+    @Get('/set-cookie')
+    setCookie(
+        @Query('name') name: string,
+        @Res() response: Response
+    ): void {
+        response.cookie('name', name)
+        response.status(200).json({
+            success: true,
+            message: 'Cookie has been set'
+        })
+    }
+
+    @Get('/get-cookie')
+    getCookie(
+        @Req() request: Request,
+        @Res() response: Response,
+    ): void {
+        const name = request.cookies['name'];
+        response.status(200).json({
+            success: true,
+            message: `Cookie 'name' is ${name}`
+        })
+    }
+
+    @Get('/remove-cookie')
+    removeCookie(
+        @Query('name') name: string,
+        @Res() response: Response
+    ): void {
+        response.clearCookie('name')
+        response.status(200).json({
+            success: true,
+            message: 'Cookie has been Removed'
+        })
+    }
+
+    // support juga untuk asynchronous dengan async sebelum method dan Promise untuk outputnya / return nya
+    @Get('/')
+    async getHome(): Promise<Record<string, any>> {
+        return {
+            success: true,
+            message: 'Welcome to the User API',
+            version: '1.0.0',
+            author: 'Muhammad Gilang Murdiyanto',
+            description: 'This API provides user related operations'
+        };
+    }
+
+    @Get('/sampleRes')
+    @Header('Content-Type', 'application/json')
+    @HttpCode(200)
+    sampleResponse(): Record<string, string> {
+        return {
+            data: 'Hello Sample Response'
+        }
+    }
+
+    @Get('/redirect')
+    @Redirect()
+    redirect(): HttpRedirectResponse {
+        return {
+            url: '/api/users',
+            statusCode: 301,
+        };
+    }
 
     @Get('/hello')
     sayHello(
@@ -11,11 +102,25 @@ export class UserController {
     ): string {
         if (!first_name) {
             return `Hello Guest!`;
-        }else if (!last_name) {
+        } else if (!last_name) {
             return `Hello ${first_name}!`;
-        }else{
+        } else {
             return `Hello ${first_name} ${last_name}`;
         }
+    }
+
+    @Get('/ipaddress')
+    getIPAddress(
+        @Ip() ip: string
+    ): string {
+        return ip
+    }
+
+    @Get('/host')
+    getHost(
+        @HostParam() host: string
+    ): string {
+        return host
     }
 
     @Post('/post')
@@ -23,7 +128,7 @@ export class UserController {
         return 'Post User';
     }
 
-    // tidak di rekomendasikan untuk langssung menggunakan express request
+    // tidak di rekomendasikan untuk langssung menggunakan express request dan lebih baik gunakan decorator
     // @Get('/:id')
     // getUserById(@Req() request : Request ) : string {
     //     return `User with ID: ${request.params.id}`;
@@ -35,7 +140,7 @@ export class UserController {
     }
 
     @Get('/get')
-    get(): string {
+    async get(): Promise<string> {
         return 'Hello NestJS User';
     }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,40 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('should can say hello firstName and lastName', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/api/users/hello')
+      .query({
+        first_name: 'Muhammad Gilang',
+        last_name: 'Murdiyanto'
+      })
+
+      expect(result.status).toBe(200);
+      expect(result.text).toBe(`Hello Muhammad Gilang Murdiyanto`);
+  })
+
+  it('should can say hello firstName and lastName', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/api/users/hello')
+      .query({
+        first_name: 'Gilang',
+        // last_name: 'Murdiyanto'
+      })
+
+      expect(result.status).toBe(200);
+      expect(result.text).toBe(`Hello Gilang!`);
+  })
+
+  it('should can say hello firstName and lastName', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/api/users/hello')
+      .query({
+        first_name: '',
+        // last_name: 'Murdiyanto'
+      })
+
+      expect(result.status).toBe(200);
+      expect(result.text).toBe(`Hello Guest!`);
+  })
 });
